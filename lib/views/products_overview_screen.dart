42d3bcf6b692b0e14d773a8a2422c29b66c24bea
hugofsd <hugo619@outlook.com>
2021-06-16 11:59:53 -0300

    Grid, tela inicial, lista de produtos

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../data/dammy_data.dart';
+import '../models/product.dart';
+
 class PrdocutsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DAMMY_PRODUCTS;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      //builder rendenizar de acordo com a demanda
+      body: GridView.builder(
+        padding: const EdgeInsets.all(11), //sair dar borda da tela
+        itemCount:
+            loadedProducts.length, // saber quantos elesmentos vai rendenizar
+        itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+        //quantidade fixa de elementos no eixo da linha, sliver com scroll,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, //dois elementos fixos por linha
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10, //eixo da linha
+          // mainAxisExtent: 10,
+        ),
+      ),
+    );
   }
 }
